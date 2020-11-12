c700cb3c004b9e0a0caa40b439d43fafa6636420
selim <furkan.tekin@data-boss.com.tr>
2020-11-13 01:39:25 +0300

    model finished

--- models/attention.py
+++ models/attention.py
@@ -1,6 +1,5 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
 
 class Attention(nn.Module):
@@ -10,24 +9,26 @@ class Attention(nn.Module):
         self.hid_conv = nn.Conv2d(in_channels=2 * hidden_dim,
                                   out_channels=1,
                                   kernel_size=3,
-                                  padding=1)
+                                  padding=1,
+                                  bias=False)
 
         self.in_conv = nn.Conv2d(in_channels=input_dim,
                                  out_channels=1,
                                  kernel_size=3,
                                  stride=1,
-                                 padding=1)
+                                 padding=1,
+                                 bias=False)
 
         self.w = nn.Linear(hidden_size[0] * hidden_size[1], attn_dim)
         self.u = nn.Linear(input_size[0] * input_size[1], attn_dim)
-        self.v = nn.Parameter(torch.rand(attn_dim), requires_grad=True)
+        self.v = nn.Linear(attn_dim, 1, bias=False)
 
     def forward(self, input_tensor, hidden):
         """
 
-        :param tuple of torch.tensor hidden: ((B, hidden, M, N), (B, hidden, M, N))
         :param torch.tensor input_tensor: (B, T, m, n)
-        :return: attention energies, (B, D)
+        :param tuple of torch.tensor hidden: ((B, hidden, M, N), (B, hidden, M, N))
+        :return: attention energies, (B, 1)
         """
         hid_conv_out = self.conv(torch.cat((hidden[0], hidden[1]), dim=1))
 
@@ -39,47 +40,6 @@ class Attention(nn.Module):
         in_vec = torch.flatten(in_conv_out, start_dim=2)
 
         # u(in_vec): (B, 1, attn_dim), w(in_vec): (B, 1, attn_dim), energy: (B, 1)
-        energy = torch.sum(self.v * (self.w(hidden_vec) + self.u(in_vec)).tanh())
+        energy = torch.sum(self.v(self.w(hidden_vec) + self.u(in_vec)).tanh())
 
         return energy
-
-
-class TemporalAttention(nn.Module):
-    def __init__(self, en_hidden_size, de_hidden_size, en_dim, de_dim, attn_dim):
-        super(TemporalAttention, self).__init__()
-
-        self.en_conv = nn.Conv2d(in_channels=en_dim,
-                                 out_channels=1,
-                                 kernel_size=3,
-                                 stride=1,
-                                 padding=1)
-
-        self.de_conv = nn.Conv2d(in_channels=2*de_dim,
-                                 out_channels=1,
-                                 kernel_size=3,
-                                 stride=1,
-                                 padding=1)
-
-        self.w = nn.Linear(en_hidden_size[0]*en_hidden_size[1], attn_dim)
-        self.u = nn.Linear(de_hidden_size[0]*de_hidden_size[1], attn_dim)
-        self.v = nn.Parameter(torch.rand(attn_dim), requires_grad=True)
-
-    def forward(self, en_hidden, de_hidden):
-        """
-        :param list of torch.tensor en_hidden:
-        :param tuple of torch.tensor de_hidden:
-        :return:
-        """
-        de_conv_out = self.de_conv(torch.cat([de_hidden[0], de_hidden[1]], dim=1))
-
-        de_vec = torch.flatten(de_conv_out, start_dim=2)
-
-        attn_energies = []
-        for hid in en_hidden:
-            en_conv_out = self.en_conv(hid)
-            en_vec = torch.flatten(en_conv_out, start_dim=2)
-
-            energy = torch.sum(self.v * (self.w(en_vec) + self.u(de_vec)).tanh())
-
-            attn_energies.append(energy)
-
--- models/model.py
+++ models/model.py
@@ -43,7 +43,11 @@ class WeatherModel(nn.Module):
                                      bias=self.decoder_params['bias'],
                                      device=self.device)
 
-        self.output_attn = Attention(input_dim=self.encoder_params['hidden_dim'])
+        self.output_attn = Attention(input_size=(15, 30),
+                                     hidden_size=self.decoder_params['hidden_size'],
+                                     input_dim=self.encoder_params['hidden_dim'],
+                                     hidden_dim=self.decoder_params['hidden_dim'],
+                                     attn_dim=temporal_attn_dim)
 
     def forward(self, input_tensor, output_tensor, flow_tensor, hidden):
         """
@@ -52,13 +56,14 @@ class WeatherModel(nn.Module):
         :type input_tensor:
         :param flow_tensor: (b, t, d, m, n)
         :type flow_tensor:
-        :param hidden_dim: [(b, d', m, n), (b, d', m, n)]
-        :type hidden_dim:
+        :param hidden: [(b, d', m, n), (b, d', m, n)]
+        :type hidden:
         :return:
         :rtype:
         """
         batch_size, win_len, dim_len, height, width = input_tensor.shape
 
+        # calculate input attention
         alpha_list = []
         for k in range(dim_len):
             # dim(x_k): (b, 256, m', n')
@@ -69,30 +74,31 @@ class WeatherModel(nn.Module):
             alpha_list.append(alpha)
 
         # dim(alpha_tensor): (B, D)
-        alpha_tensor = torch.cat(alpha_list, dim=1)
+        alpha_tensor = torch.cat(alpha_list, dim=1).squeeze()
         alpha_tensor = F.softmax(alpha_tensor, dim=1)
 
+        # calculate encoder output
         en_out = []
         for t in range(win_len):
             x_t = input_tensor[:, t].view(batch_size, dim_len, -1)
             x_tilda = x_t * alpha_tensor.unsqueeze(2)
-            x_tilda = x_tilda.view(-1, -1, height, width)
+            x_tilda = x_tilda.view(batch_size, dim_len, height, width)
 
             h, c = self.encoder(input_tensor=x_tilda, cur_state=hidden, flow_tensor=flow_tensor)
             en_out.append(h)
-        # # dim(en_out): (B, T, D, M, N)
-        # en_out = torch.stack(en_out, dim=1)
 
         de_hidden = self.decoder.init_hidden(batch_size)
+        de_out = []
+
+        # parse decoder layer and get outputs recursively
         for t in range(self.win_length):
 
             beta_list = []
             for k in range(len(en_out)):
                 beta = self.output_attn(en_out[k], de_hidden)
                 beta_list.append(beta)
-
             # dim(beta_tensor): (B, T)
-            beta_tensor = F.softmax(torch.cat(alpha_list, dim=1))
+            beta_tensor = F.softmax(torch.cat(beta_list, dim=1).squeeze())
 
             context_t = []
             for k in range(len(en_out)):
@@ -101,11 +107,13 @@ class WeatherModel(nn.Module):
                 context_t.append(h_k * beta_tensor.unsqueeze(2))
 
             context_t = torch.sum(torch.cat(context_t, 1), 1)
-            context_t = context_t.view(-1, )
+            context_t = context_t.view(batch_size, self.decoder.height, self.decoder.width)
 
-            de_in = torch.cat([context_t, output_tensor], )
+            de_in = torch.cat([context_t, output_tensor], dim=1)
+            h, c = self.decoder(de_in, de_hidden)
+            de_out.append(h)
 
+        de_out = torch.stack(de_out, dim=1)
 
-
-
+        return de_out
 
